df50cee501b2680d1030abf9bb2f90c42b65fd38
Eduardo C <eduardo.crrso@gmail.com>
2025-01-22 01:23:23 -0600

    fixed about page apostrophe errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,12 @@ const brygada = Brygada_1918({
 });
 
 export default function Page() {
+    const text1 = `I'm an avid photographer with a deep passion for capturing the world through my lens, and my specialty lies in wedding photography.`
+    
+    const text2 = `Whether it's the play of light during golden hour, the raw emotion in candid moments, or the intricate details that make each celebration unique, I love preserving the beauty and significance of these special days.`
+    
+    const text3 = `For me, photography isn't just a hobby—it's a way to connect with the world and express my creativity through visual storytelling, especially when it comes to documenting the love and joy of a couple's wedding day.`
+
     return (
         <>
             <ul className={`${brygada.className} text-md flex justify-between`}>
@@ -20,16 +26,17 @@ export default function Page() {
             <h2 className={`${brygada.className} text-5xl text-right`}>Mónica Varas</h2>
             <br />
             <p>
-                I'm an avid photographer with a deep passion for capturing the world through my lens, and my specialty lies in wedding photography.
+                {text1}
             </p>
             <br />
             <p>
-                Whether it's the play of light during golden hour, the raw emotion in candid moments, or the intricate details that make each celebration unique, I love preserving the beauty and significance of these special days.
+                {text2}
             </p>
             <br />
-            <p className="mb-8">
-                For me, photography isn't just a hobby—it's a way to connect with the world and express my creativity through visual storytelling, especially when it comes to documenting the love and joy of a couple's wedding day.
+            <p>
+                {text3}
             </p>
+            <br />
         </>
     );
 }
