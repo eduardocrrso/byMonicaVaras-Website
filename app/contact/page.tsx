f9103439a4560b97c0e4506f889d6f10a12cd726
Eduardo C <eduardo.crrso@gmail.com>
2025-01-24 00:13:59 -0600

    Completed contact form layout and animations

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -24,8 +24,7 @@ export default function Page() {
                                 className="peer"
                                 placeholder=""
                             />
-                            <label htmlFor="fiancesFirstName">
-                                {"Fiance's First Name"}<span className="required">*</span>
+                            <label htmlFor="fiancesFirstName" className="floating">{"Fiance's First Name"}<span className="required">*</span>
                             </label>
                         </li>
                         <li className="flex-1">
@@ -36,7 +35,7 @@ export default function Page() {
                                 className="peer"
                                 placeholder=""
                             />
-                            <label htmlFor="fiancesLastName">{"Fiance's Last Name"}</label>
+                            <label htmlFor="fiancesLastName" className="floating">{"Fiance's Last Name"}</label>
                         </li>
                     </div>
 
@@ -50,7 +49,7 @@ export default function Page() {
                                 className="peer"
                                 placeholder=""
                             />
-                            <label htmlFor="fianceesFirstName">{"Fiancee's First Name"}<span className="required">*</span></label>
+                            <label htmlFor="fianceesFirstName" className="floating">{"Fiancee's First Name"}<span className="required">*</span></label>
                         </li>
 
                         <li className="flex-1">
@@ -61,7 +60,7 @@ export default function Page() {
                                 className="peer"
                                 placeholder=""
                             />
-                            <label htmlFor="fianceesLastName">{"Fiancee's Last Name"}</label>
+                            <label htmlFor="fianceesLastName" className="floating">{"Fiancee's Last Name"}</label>
                         </li>
                     </div>
 
@@ -74,7 +73,7 @@ export default function Page() {
                             className="peer"
                             placeholder=""
                         />
-                        <label htmlFor="email">{"Contact Email"}<span className="required">*</span></label>
+                        <label htmlFor="email" className="floating">{"Contact Email"}<span className="required">*</span></label>
                     </li>
 
                     <li>
@@ -85,7 +84,7 @@ export default function Page() {
                             className="peer"
                             placeholder=""
                         />
-                        <label htmlFor="eventLocation">{"Event Location"}<span className="required">*</span></label>
+                        <label htmlFor="eventLocation" className="floating">{"Event Location"}<span className="required">*</span></label>
                     </li>
 
                     <li>
@@ -93,61 +92,47 @@ export default function Page() {
                             type="text"
                             id="countryOfResidence"
                             name="countryOfResidence"
-                                className="peer"
-                                placeholder=""
-                            />
-                        <label htmlFor="countryOfResidence">{"Country of Residence"}<span className="required">*</span></label>
+                            className="peer"
+                            placeholder=""
+                        />
+                        <label htmlFor="countryOfResidence" className="floating">{"Country of Residence"}<span className="required">*</span></label>
                     </li>
-                    <div className="flex justify-between flex-wrap gap-4">
+                    <li>
+                        <input
+                            type="text"
+                            id="budget"
+                            name="budget"
+                            className="peer"
+                            placeholder=""
+                        />
+                        <label htmlFor="budget" className="floating">{"What's your budget?"}</label>
+                    </li>
+
+                    <ul className="groupCheckbox">
                         <li>
-                            <p>{"What's your budget?"}</p>
-
-                            <div>
-                                <div>
-                                    <input type="radio" id="1k-2k" name="budget" value="1,000 to 1,999 USD" className="mr-2 -mt-1" />
-                                    <label htmlFor="1k-2k">{"1,000 to 1,999 USD"}</label >
-                                </div>
-                                <div>
-                                    <input type="radio" id="2k-4k" name="budget" value="2,000 to 3,999 USD" className="mr-2 -mt-1" />
-                                    <label htmlFor="2k-4k">{"2,000 to 3,999 USD"}</label>
-                                </div>
-                                <div>
-                                    <input type="radio" id="4k-6k" name="budget" value="4,000 to 5,999 USD" className="mr-2 -mt-1" />
-                                    <label htmlFor="4k-6k">{"4,000 to 5,999 USD"}</label>
-                                </div>
-                                <div>
-                                    <input type="radio" id="6k+" name="budget" value="6,000+ USD" className="mr-2 -mt-1" />
-                                    <label htmlFor="6k+">{"6,000+ USD"}</label>
-                                </div>
-                            </div>
+                            <p>{"How did you hear about me?"}</p>
                         </li>
-
-                        <ul>
-                            <li>
-                                <p>{"How did you hear about me?"}</p>
-                            </li>
-                            <li>
-                                <input type="checkbox" id="referenceVogue" name="referenceVogue" className="mr-2 -mt-1" />
-                                <label htmlFor="referenceVogue">{"Vogue"}</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" id="referenceInstagram" name="referenceInstagram" className="mr-2 -mt-1" />
-                                <label htmlFor="referenceInstagram">{"Instagram"}</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" id="referenceRecommendation" name="referenceRecommendation" className="mr-2 -mt-1" />
-                                <label htmlFor="referenceRecommendation">{"Recommendation"}</label>
-                            </li>
-                            <li>
-                                <input type="checkbox" id="referenceGoogle" name="referenceGoogle" className="mr-2 -mt-1" />
-                                <label htmlFor="referenceGoogle">{"Google"}</label>
-                            </li>
-                            <li className="flex mt-4 ml-6">
-                                <label htmlFor="referenceOther" className="mt-[7px]">{"Other"}</label>
-                                <input type="text" id="referenceOther" name="referenceOther" className="w-[9rem]" />
-                            </li>
-                        </ul>
-                    </div>
+                        <li>
+                            <input type="checkbox" id="referenceVogue" name="referenceVogue" className="mr-2 -mt-1" />
+                            <label htmlFor="referenceVogue">{"Vogue"}</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="referenceInstagram" name="referenceInstagram" className="mr-2 -mt-1" />
+                            <label htmlFor="referenceInstagram">{"Instagram"}</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="referenceGoogle" name="referenceGoogle" className="mr-2 -mt-1" />
+                            <label htmlFor="referenceGoogle">{"Google"}</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="referenceRecommendation" name="referenceRecommendation" className="mr-2 -mt-1" />
+                            <label htmlFor="referenceRecommendation">{"Recommendation"}</label>
+                        </li>
+                        <li className="flex mt-6">
+                            <input type="text" id="referenceOther" name="referenceOther" className="max-w-[13rem] peer mt-2" placeholder="" />
+                            <label htmlFor="referenceOther" className="floating mt-2">{"Other"}</label>
+                        </li>
+                    </ul>
                     <li className="hidden">
                         <label htmlFor="">{"If you're human, keep this blank"}</label>
                         <input name="bot-field" />
@@ -155,13 +140,23 @@ export default function Page() {
                 </ul>
 
                 <ul>
-                    <li className="mt-12">
-                        <label htmlFor="message" aria-required="true" className="mt-12">{"Tell me more about your plans"}</label>
-                        <textarea name="message" id="message" cols={30} rows={4} className="min-w-full h-24 focus:mt-0"></textarea>
+                    <li className="mt-16">
+                        <textarea name="message" id="message" cols={30} rows={4} className="min-w-full h-24 border-t-[1px] peer focus:border-t-[2px]" placeholder=""></textarea>
+                        <label htmlFor="message" aria-required="true" className="absolute left-0 -top-4 text-sm
+                                                                                peer-placeholder-shown:text-base
+                                                                                peer-placeholder-shown:text-gray
+                                                                                peer-placeholder-shown:top-2
+                                                                                peer-focus:-top-6
+                                                                                peer-focus:text-sm
+                                                                                peer-focus:text-foreground
+                                                                                transition-all"
+                        >
+                            {"Tell me more about your plans"}
+                        </label>
                     </li>
 
                     <li className="flex justify-center my-6">
-                        <button type="submit" className="underline font-bold">{"Submit>>"}</button>
+                        <button type="submit" className="underline font-bold text-xl">{"Submit>>"}</button>
                     </li>
                 </ul>
             </form>
