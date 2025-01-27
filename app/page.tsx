169e123c4f54236641e89bca64902574449b0460
Eduardo Carnero <eduardo.crrso@gmail.com>
2025-01-27 12:02:41 -0600

    changed home images. not longer links

--- app/page.tsx
+++ app/page.tsx
@@ -20,154 +20,212 @@ export default async function Home() {
         <main>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
 
-                <Link
-                    href={"/photoshoot/01"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-01/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li className="flex">
-                                <p>
-                                    Rome, Italy
-                                </p>
-                            </li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-01.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
 
-                <Link
-                    href={"/photoshoot/02"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-02/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
 
-                <Link
-                    href={"/photoshoot/03"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-03/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-02.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
 
-                <Link
-                    href={"/photoshoot/04"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-04/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
 
-                <Link
-                    href={"/photoshoot/05"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-05/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-03.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
 
-                <Link
-                    href={"/photoshoot/06"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-06/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
 
-                <Link
-                    href={"/photoshoot/07"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-07/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-04.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
 
-                <Link
-                    href={"/photoshoot/08"}
-                >
-                    <div className="h-full flex flex-col">
-                        <Image
-                            src={"/photoshoots/photoshoot-08/wedding-photo-01.jpeg"}
-                            alt="Guys in a wedding"
-                            height={height}
-                            width={width}
-                            className="h-full object-cover"
-                        />
-                        <ul className={`${brygada.className} text-sm flex justify-between`}>
-                            <li>Rome, Italy</li>
-                            <li>2024</li>
-                        </ul>
-                    </div>
-                </Link>
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
 
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-05.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
+
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-06.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
+
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-07.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
+
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-08.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
+
+                <div className="h-full flex flex-col">
+                    <Image
+                        src={"/photos/wedding-photo-09.jpeg"}
+                        alt="Wedding photo"
+                        height={height}
+                        width={width}
+                        className="h-full object-cover"
+                    />
+                    <ul className={`${brygada.className} text-sm flex justify-between`}>
+                        <li className="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-[1.15rem] -ml-[0.125rem]">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                            </svg>
+
+                            <p>
+                                Rome, Italy
+                            </p>
+                        </li>
+                        <li>2024</li>
+                    </ul>
+                </div>
             </div>
         </main>
     );
