44c8f76ac1e4c819e321f7182cd5e814110870c1
Eduardo Carnero <eduardo.crrso@gmail.com>
2025-07-25 13:02:25 -0600

    added enter animation for home pictures and created react picture components

---
+++ app/components/picture.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import * as motion from "motion/react-client";
+
+const height = 1350;
+const width = 1080;
+
+export function Picture({ src }: { src: string }) {
+    return (
+        <motion.div initial={{ translateY: -8, opacity: 0}}
+                    whileInView={{ translateY: 0, opacity: 1}}
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.6 }}
+                    className="h-full flex flex-col">
+            <Image
+                src={ src }
+                alt="Wedding photo"
+                height={height}
+                width={width}
+                className="h-full object-cover"
+            />
+        </motion.div>
+    );
+}
