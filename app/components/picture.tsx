c89f272b18190d742b8de7bb60fd3fbe9295dec4
Eduardo Carnero <eduardo.crrso@gmail.com>
2025-07-30 14:50:34 -0600

    Added event handler for Image>onLoad to start animations on image load

--- app/components/picture.tsx
+++ app/components/picture.tsx
@@ -1,22 +1,35 @@
+"use client"
+
 import Image from "next/image";
 import * as motion from "motion/react-client";
+import { useState } from "react";
 
 const height = 1350;
 const width = 1080;
 
 export function Picture({ src }: { src: string }) {
+    const [loading, setLoading] = useState(true);
+
+    function handleLoading() {
+        setLoading(false);
+    }
+
     return (
-        <motion.div initial={{ translateY: -8, opacity: 0}}
-                    whileInView={{ translateY: 0, opacity: 1}}
-                    viewport={{ once: true }}
-                    transition={{ duration: 0.6 }}
-                    className="h-full flex flex-col">
+        <motion.div
+            initial={{ translateY: -8, opacity: 0 }}
+            animate={{
+                translateY: loading ? -8 : 0,
+                opacity: loading ? 0 : 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6 }}
+            className="h-full flex flex-col">
             <Image
-                src={ src }
+                src={src}
                 alt="Wedding photo"
                 height={height}
                 width={width}
                 className="h-full object-cover"
+                onLoad={handleLoading}
             />
         </motion.div>
     );
