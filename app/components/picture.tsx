f364f85b360df74413392af4e1e52ee456134dae
Eduardo Carnero <eduardo.crrso@gmail.com>
2025-08-01 15:26:37 -0600

    mend

--- app/components/picture.tsx
+++ app/components/picture.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import * as motion from "motion/react-client";
 import { useState } from "react";
+import { rgba } from "motion";
 
 const height = 1350;
 const width = 1080;
@@ -17,17 +18,24 @@ export function Picture({ src }: { src: string }) {
     return (
         <motion.div
             initial={{ translateY: -8 }}
-            whileInView={{ translateY: loading ? -8 : 0 }}
+            whileInView={{ translateY: loading ? -4 : 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.6 }}
             className="h-full flex flex-col aspect-[0.65]">
             <motion.div
-                className="h-full flex flex-col bg-slate-400">
+                animate={{ backgroundColor: ["#000000", "#ffffff"] }}
+                transition={{
+                    duration: 1.2,
+                    repeat: loading ? Infinity : 0,
+                    repeatType: "mirror",
+                    ease: "easeInOut"
+                }}
+                className="h-full flex flex-col">
                 <motion.div
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: loading ? 0 : 1 }}
                     viewport={{ once: true }}
-                    transition={{ duration: 1.6 }}
+                    transition={{ duration: 0.6 }}
                     className="h-full flex flex-col">
                     <Image
                         src={src}
