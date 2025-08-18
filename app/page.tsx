56f9ca00a84a555bc826578f28a70a218fe2c784
Eduardo Carnero <eduardo.crrso@gmail.com>
2025-08-18 12:27:56 -0600

    Changed home photos

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,6 @@ export default async function Home() {
     return (
         <main>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
-                <Picture src="/photos/wedding-photo-10.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-11.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-12.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-13.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-14.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-15.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-01.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-02.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-03.webp" width={width} height={height} aspectRatio={aspectRatio}/>
@@ -30,6 +23,15 @@ export default async function Home() {
                 <Picture src="/photos/wedding-photo-07.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-08.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-09.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-10.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-11.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-12.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-13.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-14.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-15.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-17.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-18.webp" width={width} height={height} aspectRatio={aspectRatio}/>
             </div>
         </main>
     );
