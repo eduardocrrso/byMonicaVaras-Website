7d6f0197c2e630c5168a603fc82d445df7523eca
Eduardo C <eduardo.crrso@gmail.com>
2025-08-17 22:19:42 -0600

    Changed home pictures aspect ratio

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,27 @@ export const metadata: Metadata = {
 export default async function Home() {
     const height = 1350;
     const width = 1080;
+    const aspectRatio = 0.6666;
 
     return (
         <main>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
-                <Picture src="/photos/wedding-photo-10.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-11.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-12.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-13.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-14.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-15.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-01.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-02.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-03.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-04.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-05.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-06.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-07.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-08.webp" width={width} height={height} aspectRatio={0.65}/>
-                <Picture src="/photos/wedding-photo-09.webp" width={width} height={height} aspectRatio={0.65}/>
+                <Picture src="/photos/wedding-photo-10.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-11.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-12.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-13.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-14.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-15.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-01.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-02.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-03.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-04.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-05.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-06.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-07.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-08.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-09.webp" width={width} height={height} aspectRatio={aspectRatio}/>
             </div>
         </main>
     );
