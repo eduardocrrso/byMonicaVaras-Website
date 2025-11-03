c6585927aef7215bec6ee9e51940d1a9b8f91975
Eduardo Carnero <eduardo@moonkyte.dev>
2025-11-02 20:50:39 -0600

    changed home photos order

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,9 @@ export default async function Home() {
     return (
         <main>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
+                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-17.webp" width={width} height={height} aspectRatio={aspectRatio}/>
+                <Picture src="/photos/wedding-photo-18.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-01.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-02.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-03.webp" width={width} height={height} aspectRatio={aspectRatio}/>
@@ -29,9 +32,6 @@ export default async function Home() {
                 <Picture src="/photos/wedding-photo-13.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-14.webp" width={width} height={height} aspectRatio={aspectRatio}/>
                 <Picture src="/photos/wedding-photo-15.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-16.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-17.webp" width={width} height={height} aspectRatio={aspectRatio}/>
-                <Picture src="/photos/wedding-photo-18.webp" width={width} height={height} aspectRatio={aspectRatio}/>
             </div>
         </main>
     );
